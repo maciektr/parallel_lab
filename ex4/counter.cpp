bd38fde5b52769c3dd144793d0b33e7051b6bc94
maciektr <mtratnowiecki@gmail.com>
2022-05-24 01:05:39 +0200

    Print counting result in serial

--- ex4/counter.cpp
+++ ex4/counter.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main(int argc, char** argv){
+    const string result_filename = "output.txt";
     std::ifstream file(argv[1]);
     unordered_map<string, int> counter;
     if ( file.is_open() ) {    
@@ -13,4 +14,8 @@ int main(int argc, char** argv){
                 insert_result.first->second = (insert_result.first->second)+1; 
         }         
     }
+    ofstream result_file(result_filename);
+    for (auto const& entry : counter)
+        result_file<<entry.first<<':'<<entry.second<<endl;
+    result_file.close();
 }
