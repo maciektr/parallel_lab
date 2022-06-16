cdb93970190f855d95ae8ea3cbd1ab5eec3ebef4
Maciej Tratnowiecki <mtratnowiecki@gmail.com>
2022-06-16 15:26:27 +0200

    Add Lab2 implementation

--- cuda/Lab2/image_scailing/image_scailing.cu
+++ cuda/Lab2/image_scailing/image_scailing.cu
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include"scrImagePgmPpmPackage.h"
+#include "gputimer.h"
 
 //Kernel which calculate the resized image
 __global__ void createResizedImage(unsigned char *imageScaledData, int scaled_width, float scale_factor, cudaTextureObject_t texObj)
@@ -14,14 +15,29 @@ __global__ void createResizedImage(unsigned char *imageScaledData, int scaled_wi
 
 int main(int argc, char*argv[])
 {
+	// Scaling ratio, Block size, Grid size
 	int height=0, width =0, scaled_height=0,scaled_width=0;
 	//Define the scaling ratio	
 	float scaling_ratio=0.5;
+	
+	if(argc > 1)
+		sscanf(argv[1],"%f",&scaling_ratio);
+
+	int block_size = 32;
+
+	if (argc > 3) 
+        block_size = atoi(argv[3]);
+
+	GpuTimer timer;
+    timer.Start();
+
 	unsigned char*data;
 	unsigned char*scaled_data,*d_scaled_data;
 
-	char inputStr[1024] = {"aerosmith-double.pgm"};
-	char outputStr[1024] = {"aerosmith-double-scaled.pgm"};
+	// char inputStr[1024] = {"aerosmith-double.pgm"};
+	// char outputStr[1024] = {"aerosmith-double-scaled.pgm"};
+	char inputStr[1024] = {"voyager2.pgm"};
+	char outputStr[1024] = {"voyager2-scaled.pgm"};
 	cudaError_t returnValue;
 
 	//Create a channel Description to be used while linking to the tecture
@@ -31,17 +47,18 @@ int main(int argc, char*argv[])
 
 	get_PgmPpmParams(inputStr, &height, &width);	//getting height and width of the current image
 	data = (unsigned char*)malloc(height*width*sizeof(unsigned char));
-	printf("\n Reading image width height and width [%d][%d]", height, width);
+	// printf("\n Reading image width height and width [%d][%d]", height, width);
 	scr_read_pgm( inputStr , data, height, width );//loading an image to "inputimage"
 
 	scaled_height = (int)(height*scaling_ratio);
 	scaled_width = (int)(width*scaling_ratio);
 	scaled_data = (unsigned char*)malloc(scaled_height*scaled_width*sizeof(unsigned char));
-	printf("\n scaled image width height and width [%d][%d]", scaled_height, scaled_width);
+	// printf("\n scaled image width height and width [%d][%d]", scaled_height, scaled_width);
 
 	//Allocate CUDA Array
  	returnValue = cudaMallocArray( &cu_array, &channelDesc, width, height);
-	returnValue = (cudaError_t)(returnValue | cudaMemcpy( cu_array, data, height * width * sizeof(unsigned char), cudaMemcpyHostToDevice));
+	// returnValue = (cudaError_t)(returnValue | cudaMemcpy( cu_array, data, height * width * sizeof(unsigned char), cudaMemcpyHostToDevice));
+	returnValue = (cudaError_t)(returnValue | cudaMemcpyToArray( cu_array, 0, 0, data, height * width * sizeof(unsigned char), cudaMemcpyHostToDevice));
 
 	if(returnValue != cudaSuccess)
 		printf("\n Got error while running CUDA API Array Copy");
@@ -69,9 +86,11 @@ int main(int argc, char*argv[])
 	
 	cudaMalloc(&d_scaled_data, scaled_height*scaled_width*sizeof(unsigned char) );
 
-	dim3 dimBlock(32, 32,1);
-	dim3 dimGrid(scaled_width/dimBlock.x,scaled_height/dimBlock.y,1);
-	printf("\n Launching grid with blocks [%d][%d] ", dimGrid.x,dimGrid.y);
+
+	dim3 dimBlock(block_size, block_size,1);
+	dim3 dimGrid(scaled_width/dimBlock.x + 1,scaled_height/dimBlock.y + 1,1);
+
+	// printf("\n Launching grid with blocks [%d][%d] ", dimGrid.x,dimGrid.y);
 
 	createResizedImage<<<dimGrid, dimBlock>>>(d_scaled_data,scaled_width,1/scaling_ratio, texObj);
 
@@ -94,6 +113,12 @@ int main(int argc, char*argv[])
 		free(scaled_data);
 	if(d_scaled_data!=NULL)
 		cudaFree(d_scaled_data);
-	
+
+    timer.Stop();
+	float timer_result = timer.Elapsed();
+
+    // printf("height;width;scaled_height;scaled_width;time;block_size\n");
+    printf("%d, %d, %d, %d, %f, %d\n", height, width, scaled_height, scaled_width, timer_result, block_size);
+
 	return 0;
 }
--- cuda/Lab2/matrix_transpose/matrix_transpose_1.cu
+++ cuda/Lab2/matrix_transpose/matrix_transpose_1.cu
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include "gputimer.h"
 
-#define N 2048
+// #define N 2048
 #define BLOCK_SIZE 32 
 
-__global__ void matrix_transpose_naive(int *input, int *output) {
+__global__ void matrix_transpose_naive(int *input, int *output, int N) {
 
 	int indexX = threadIdx.x + blockIdx.x * blockDim.x;
 	int indexY = threadIdx.y + blockIdx.y * blockDim.y;
@@ -18,7 +19,7 @@ __global__ void matrix_transpose_naive(int *input, int *output) {
 	// output[index] = input[transposedIndex];
 }
 
-__global__ void matrix_transpose_shared(int *input, int *output) {
+__global__ void matrix_transpose_shared(int *input, int *output, int N) {
 
 	__shared__ int sharedMemory [BLOCK_SIZE] [BLOCK_SIZE];
 
@@ -47,12 +48,12 @@ __global__ void matrix_transpose_shared(int *input, int *output) {
 }
 
 //basically just fills the array with index.
-void fill_array(int *data) {
+void fill_array(int *data, int N) {
 	for(int idx=0;idx<(N*N);idx++)
 		data[idx] = idx;
 }
 
-void print_output(int *a, int *b) {
+void print_output(int *a, int *b, int N) {
 	printf("\n Original Matrix::\n");
 	for(int idx=0;idx<(N*N);idx++) {
 		if(idx%N == 0)
@@ -66,16 +67,44 @@ void print_output(int *a, int *b) {
 		printf(" %d ",  b[idx]);
 	}
 }
-int main(void) {
+
+int main(int argc, char *argv[]) {
+	// argv = N ; Type ; Grid size ; Block size
+	int N =0;
+
+    if (argc > 1) {
+        N = atoi(argv[1]);
+    } else {
+        printf("Please pass number of elements as command line argument.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    bool shared = false;
+    if (argc > 2 && strcmp(argv[2], "shared") == 0){
+		shared = true;
+    }
+
+	int the_block_size = BLOCK_SIZE;
+	int the_grid_size = N / BLOCK_SIZE;
+
+	if (argc > 3) 
+        the_grid_size = atoi(argv[4]);
+
+	if (argc > 4) 
+        the_block_size = atoi(argv[3]);
+
 	int *a, *b;
-        int *d_a, *d_b; // device copies of a, b, c
+    int *d_a, *d_b; // device copies of a, b, c
 
 	int size = N * N *sizeof(int);
 
 	// Alloc space for host copies of a, b, c and setup input values
-	a = (int *)malloc(size); fill_array(a);
+	a = (int *)malloc(size); fill_array(a, N);
 	b = (int *)malloc(size);
 
+	GpuTimer timer;
+    timer.Start();
+	
 	// Alloc space for device copies of a, b, c
 	cudaMalloc((void **)&d_a, size);
 	cudaMalloc((void **)&d_b, size);
@@ -84,26 +113,31 @@ int main(void) {
 	cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
 
-	dim3 blockSize(BLOCK_SIZE,BLOCK_SIZE,1);
-	dim3 gridSize(N/BLOCK_SIZE,N/BLOCK_SIZE,1);
+	dim3 blockSize(the_block_size,the_block_size,1);
+	dim3 gridSize(the_grid_size,the_grid_size,1);
 
-	matrix_transpose_naive<<<gridSize,blockSize>>>(d_a,d_b);
+	if(!shared)
+	matrix_transpose_naive<<<gridSize,blockSize>>>(d_a,d_b, N);
 	
-	// Copy result back to host
-	// cudaMemcpy(b, d_b, size, cudaMemcpyDeviceToHost);
-	// print_output(a,b);
-
-	matrix_transpose_shared<<<gridSize,blockSize>>>(d_a,d_b);
+	if(shared)
+	matrix_transpose_shared<<<gridSize,blockSize>>>(d_a,d_b, N);
 
 	// Copy result back to host
 	cudaMemcpy(b, d_b, size, cudaMemcpyDeviceToHost);
 	// print_output(a,b);
 
+    cudaFree(d_a);
+	cudaFree(d_b); 
+
+    timer.Stop();
+	float timer_result = timer.Elapsed();
+
 	// terminate memories
 	free(a);
 	free(b);
-    cudaFree(d_a);
-	cudaFree(d_b); 
+
+    // printf("size;time;type;grid_size;block_size\n");
+    printf("%d, %f, %s, %d, %d\n", N, timer_result, (shared ? "shared" : "naive"), the_grid_size, the_block_size);
 
 	return 0;
 }
